4086213fc8e66c7b5d6d5944d7eb6a5bc76d52d7
adsl <adlibrarywebsite@gmail.com>
2020-09-03 17:14:00 -0700

    Update4

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 function date(){
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
     var yyyy = today.getFullYear();
 
     today = mm + '/' + dd + '/' + yyyy;
     document.getElementById("date").innerHTML=today
     console.log("date function loaded.")
+}
+
+var x = document.getElementById("location");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude;
 }
